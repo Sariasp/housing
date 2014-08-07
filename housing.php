cb354f645ad5cde7a2a2187019bafaf5f9c8c501
Sariasp <webtechsp@yahoo.com>
2014-08-07 15:03:44 -0400

    housing update
    housing update

--- housing.php
+++ housing.php
@@ -1,86 +1,65 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Untitled Document</title>
-</head>
 
-<body>
-
-<form action="housing.php" method="post">
-<select name="state" size="1">
-  <option value="AL">Alabama</option>
-  <option value="AZ">Arizona</option>
-  <option value="AR">Arkansas</option>
-  <option value="CA">California</option>
-  <option value="CO">Colorado</option>
-  <option value="CT">Connecticut</option>
-  <option value="DE">Delaware</option>
-  <option value="DC">Dist of Columbia</option>
-  <option value="FL">Florida</option>
-  <option value="GA">Georgia</option>
-  <option value="HI">Hawaii</option>
-  <option value="ID">Idaho</option>
-  <option value="IL">Illinois</option>
-  <option value="IN">Indiana</option>
-  <option value="IA">Iowa</option>
-  <option value="KS">Kansas</option>
-  <option value="KY">Kentucky</option>
-  <option value="LA">Louisiana</option>
-  <option value="ME">Maine</option>
-  <option value="MD">Maryland</option>
-  <option value="MA">Massachusetts</option>
-  <option value="MI">Michigan</option>
-  <option value="MN">Minnesota</option>
-  <option value="MS">Mississippi</option>
-  <option value="MO">Missouri</option>
-  <option value="MT">Montana</option>
-  <option value="NE">Nebraska</option>
-  <option value="NV">Nevada</option>
-  <option value="NH">New Hampshire</option>
-  <option value="NJ">New Jersey</option>
-  <option value="NM">New Mexico</option>
-  <option value="NY">New York</option>
-  <option value="NC">North Carolina</option>
-  <option value="ND">North Dakota</option>
-  <option value="OH">Ohio</option>
-  <option value="OK">Oklahoma</option>
-  <option value="OR">Oregon</option>
-  <option value="PA">Pennsylvania</option>
-  <option value="RI">Rhode Island</option>
-  <option value="SC">South Carolina</option>
-  <option value="SD">South Dakota</option>
-  <option value="TN">Tennessee</option>
-  <option value="TX">Texas</option>
-  <option value="UT">Utah</option>
-  <option value="VT">Vermont</option>
-  <option value="VA">Virginia</option>
-  <option value="WA">Washington</option>
-  <option value="WV">West Virginia</option>
-  <option value="WI">Wisconsin</option>
-  <option value="WY">Wyoming</option>
-</select>
-<input type="submit" value="Select State" />
-</form>
-<?php $con = mysqli_connect('localhost', 'root', 'cbppdb','housing') or die('Connection error');?>
-
-<?php if (isset($state)){  $state=$_POST['state'];                   
-
-$query= 'SELECT * FROM sheet1 WHERE SUBSTRING(agency_code,1,2 ) = '.$state;
+<?php
+$state = $_GET['s'];
+$year = $_GET['year'];
+$con = mysqli_connect('localhost', 'root', 'cbppdb','housing') or die('Connection error');
+$query= 'SELECT * FROM sheet1 WHERE SUBSTRING(agency_code,1,2 ) = "'.$state.'"';
 $result = mysqli_query($con,$query);
 
+echo "<select>";
 
- }?>
-
-
-
-
-
-<table>
-
-<tr><th>Agency Name</th><th>Code</th><th colspan="9">% of Authorized Vouchers in Use</th></tr>
-<tr><th></th><th></th><th>2004</th><th>2005</th><th>2007</th><th>2008></th><th>2009</th><th>2010</th><th>2011</th><th>2012</th><th>2013</th></tr></table>
+while($row = mysqli_fetch_array($result)) {
+  echo "<option>". $row['agency_name'] . "</option>";
+   }
+echo "</select>";
 
 
-</body>
-</html>
+
+
+
+
+
+
+/*
+
+
+
+if(!in_array("all", $year)){
+	$query .=  " AND (";
+	for($i=0; $i<count($year);$i++){
+		$state[$i]=	$state	
+	}
+	
+}
+
+
+
+
+
+echo "<table border='1'>
+<tr><th>Agency Name</th><th>Code</th><th>UML</th><th>UMA</th><th>Utilization Rate</th></tr>
+";
+
+while($row = mysqli_fetch_array($result)) {
+  echo "<tr>";
+  echo "<td>" . $row['agency_name'] . "</td>";
+  echo "<td>" . $row['agency_code'] . "</td>";
+  echo "<td>" . $row['Age'] . "</td>";
+  echo "<td>" . $row['Hometown'] . "</td>";
+  echo "<td>" . $row['Job'] . "</td>";
+  echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($con);
+*/
+?>
+
+
+
+
+
+
+
+
+
---
+++ housing2.php
@@ -0,0 +1,172 @@
+<?php
+
+$con = mysqli_connect('localhost', 'root', 'cbppdb','housing') or die('Connection error');
+
+$state = $_GET['s'];	
+//$year=$_GET['y'];
+$count=0;
+
+//Display Agency drop-down after state is slected 
+$query= 'SELECT * FROM sheet1 WHERE SUBSTRING(agency_code,1,2 ) = "'.$state.'" ORDER BY agency_name';
+$result = mysqli_query($con,$query);
+if(!isset($_GET['a'])){
+
+	echo "Agency: <select name=\"agencySelector\" id=\"agencySelector\" onchange=\"agencyInfo(this.value)\"  multiple=\"multiple\"> 
+			<option value=\"\">SELECT AN AGENCY</option>";
+	
+	while($row = mysqli_fetch_array($result)) {
+	  	echo "<option value='".$row['agency_code']."'>". $row['agency_name'] . "</option>";
+	}
+	echo "</select>";
+}
+
+	
+//Display Table of Results After recieving agency data
+if (isset($_GET['a'])){
+	$agency=$_GET['a'];
+	$agencyArray=explode("-", $agency);
+	$stateUMA=0;
+	$stateUML=0;
+	
+	$years=$_GET['y'];
+	$yearArray=explode("x", $years);
+	
+	$stateUMA_A = array();
+	$stateUML_A = array();
+	$avgUMA_A=array();
+	$avgUML_A=array();
+	$avgUR_A=array();
+	
+	$row_cnt = $result->num_rows;
+	
+	$resultsArr = array();
+	
+	while($row = mysqli_fetch_array($result)) {
+		array_push($resultsArr,$row);
+	}
+			
+		foreach($yearArray as $year){
+			
+			for($i=0;$i<count($resultsArr);$i++) {
+				$row = $resultsArr[$i];
+				
+				$uma= (float) str_replace(',' , '',$row['uma_cy'.$year]); //strip commas out of data and convert to float
+				$uml= (float)str_replace(',' , '',$row['uml_cy'.$year]);
+		 
+		 		 //Calculate sum of UMA and UML for the State
+				 $stateUMA=$stateUMA+$uma; 
+				 $stateUML=$stateUML+$uml;	
+				// echo "Year: ".$year." UMA: ".$uma." StateUMA: ".$stateUMA. "</br/>";
+				 
+			  }
+			
+		array_push($stateUMA_A,$stateUMA); 
+		array_push($stateUML_A,$stateUML);
+		
+		$stateUMA=0;
+		$stateUML=0;
+		}
+	
+		
+	   //Calculate average UMA, UML and UR for State
+	 for ($m=0;$m<count($yearArray);$m++) {
+		 $avgUMA=number_format($stateUMA_A[$m] / $row_cnt,2); 
+		array_push($avgUMA_A,$avgUMA);
+		 
+		 $avgUML=number_format($stateUML_A[$m] / $row_cnt,2); 
+		array_push($avgUML_A,$avgUML);
+		
+		 
+		 $avgUR=number_format($avgUML/$avgUMA, 2);
+		array_push($avgUR_A,$avgUR);
+	 }
+	
+		
+	$query2= 'SELECT * FROM sheet1 WHERE ';
+	 for ($i=0;$i<count($agencyArray);$i++) {
+		$query2 .= "agency_code = '" . $agencyArray[$i]."'";
+			if ($i<count($agencyArray)-1){
+				 $query2 .= " OR ";
+			 }
+	 }
+	
+	 $query2 .=  ' ORDER BY agency_name';
+	
+	$result2 = mysqli_query($con,$query2);
+	$row_cnt2 = $result2->num_rows;
+
+	$returnAgency=array(); 
+	$returnAgencyName=array();
+	$returnUMA=array();
+	$returnUML=array();
+	$returnUR=array();
+	     $count=0;
+			while($row2 = mysqli_fetch_array($result2)) {
+	          array_push($returnAgency, $row2['agency_code']);
+			  array_push($returnAgencyName,$row2['agency_name']);
+			
+			  foreach($yearArray as $year){
+				  //strip commas out of data and convert to float
+				  $uma=  $row2['uma_cy'.$year];
+				  $uml= $row2['uml_cy'.$year];
+				  $ur= (float) str_replace(',' , '',$uml)/(float) str_replace(',' , '',$uma);
+				  $ur=number_format($ur,2);
+				  
+				   array_push($returnUMA, $uma);
+			   	   array_push($returnUML, $uml);
+				   array_push($returnUR,$ur);
+				   
+       			}
+		   }
+
+	echo "<table id=\"table1\" ><tr ><th >Agency Name</th><th >Code</th>";
+	
+	//Loop to display all of years selected
+		foreach($yearArray as $year){
+			echo "<td>";
+			echo"<table border=\"0\" class=\"table2\" ><tr><th colspan=\"3\">20".$year."</th></tr>";
+			echo "<tr ><th>UMA</th><th>UML</th><th>UR</th></tr></table>";
+			echo"</td>";
+	}
+	echo"</tr>";
+
+$num=0;
+		for($n=0; $n<$row_cnt2;$n++){
+			 echo "<tr>";
+	   		 echo "<td>" . $returnAgencyName[$n]  . "</td>";
+	    	 echo "<td>" . $returnAgency[$n] . "</td>";
+			
+			for($k=0; $k<count($yearArray); $k++){
+				
+				echo "<td><table class=\"table2\" ><tr><td>" . $returnUMA[$num] . "</td>";
+	   	 		echo "<td>" .$returnUML[$num] . "</td>";
+	    		echo "<td>" . $returnUR[$num] . "</td></tr></table></td>";
+	     		$num++;
+			}
+			
+		 }
+		echo "</tr>";
+ 	 
+
+	echo"<tr id=\"state\">
+		<td>State</td><td></td>
+		";
+		for($j=0; $j<count($yearArray); $j++){
+			echo"<td><table class=\"table2\" ><tr><td>".$avgUMA_A[$j]. "</td>
+			<td>".$avgUML_A[$j]."</td><td>".$avgUR_A[$j]."	</td></tr></table></td>";
+		 }
+		echo"</tr></table>";
+	
+}
+
+
+
+
+?>
+
+
+
+
+
+
+
